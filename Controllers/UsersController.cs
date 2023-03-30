8ca89297d52339db38dea1d8228ccf4f67dd86a9
NoSelection <ahmetakalpler@gmail.com>
2023-03-30 15:00:44 +0300

    1

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -21,8 +21,29 @@ namespace LibraryManagementSystemAPI.Controllers
         public async Task<ActionResult<List<Users>>> GetAllUsers()
         {
             using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
-            var users = await connection.QueryAsync<Users>("select * from Library_USERS");
+            IEnumerable<Users> users = await SelectAllUsers(connection);
             return Ok(users);
+        }
+
+        private static async Task<IEnumerable<Users>> SelectAllUsers(SqlConnection connection)
+        {
+            return await connection.QueryAsync<Users>("select * from Library_USERS");
+        }
+
+        [HttpGet("{UserID}")]
+        public async Task<ActionResult<List<Users>>> GetUser(int UserID)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var user = await connection.QueryFirstAsync<Users>("select * from Users where ID = @ID",
+                new {ID = UserID });
+            return Ok(user);
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<Users>>> CreateUser(Users user)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("insert into Library_USERS(First_Name, Last_Name, Phone, Email, Password, Age) values (@FirstName, @LastName, @Phone, @Email, @Password, @Age)", user);
+            return Ok(await SelectAllUsers(connection));
         }
     }
 }
